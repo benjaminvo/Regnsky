fed56afa6fc0ed925b318385bf2f39c8f32434da
Benjamin Ottensten <benjamin@ungtriumf.dk>
2015-12-26 22:28:51 +0100

    Hide category from entry footer

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -48,10 +48,10 @@ function regnsky_entry_footer() {
 	// Hide category and tag text for pages.
 	if ( 'post' === get_post_type() ) {
 		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( esc_html__( ', ', 'regnsky' ) );
-		if ( $categories_list && regnsky_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'regnsky' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-		}
+		// $categories_list = get_the_category_list( esc_html__( ', ', 'regnsky' ) );
+		// if ( $categories_list && regnsky_categorized_blog() ) {
+		// 	printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'regnsky' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+		// }
 
 		/* translators: used between list items, there is a space after the comma */
 		$tags_list = get_the_tag_list( '', esc_html__( '', 'regnsky' ) );
