6c2dd87695c92f9ceff1631c06ba48b493b91db3
Benjamin Ottensten <benjamin@ungtriumf.dk>
2015-12-31 12:35:06 +0100

    Restructured entry action section (share, comment, read)

--- single.php
+++ single.php
@@ -17,42 +17,63 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content', get_post_format() ); ?>
 
-			<hr>
+			<!-- Bottom of entry - Share, comment and read more -->
+			<div class="entry-actions entry-social row">
+				<div class="col-sm-3">
+					<hr>
+					<!-- Display first header on extra small screens, second header on larger -->
+					<h4 class="entry-actions-header visible-xs">Del og kommentér</h4>
+					<h4 class="entry-actions-header hidden-xs">Del</h4>
 
-			<h4>Del og kommentér</h4>
-
-			<div class="share-post">
-				<div class="fb">
-					<a href="#" onclick="return false;">
-						<img class="icon-fb" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icon-fb.svg" alt="Facebook share"><span class="h6">Del (12)</span>
-					</a>
+					<div class="share-post">
+						<div class="fb">
+							<a href="#" onclick="return false;">
+								<img class="icon-fb" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icon-fb.svg" alt="Facebook share"><span class="h6">Del (12)</span>
+							</a>
+						</div>
+						
+						<div class="twitter">
+							<a href="#" onclick="return false;">
+								<img class="icon-twitter" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icon-twitter.svg" alt="Twitter share"><span class="h6">Tweet (1)</span>
+							</a>
+						</div>
+					</div>
 				</div>
-				
-				<div class="twitter">
-					<a href="#" onclick="return false;">
-						<img class="icon-twitter" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icon-twitter.svg" alt="Twitter share"><span class="h6">Tweet (1)</span>
-					</a>
+
+				<div class="col-sm-9">
+					<!-- Do not display header elements on extra small screens -->
+					<hr class="hidden-xs">
+					<h4 class="hidden-xs entry-section-header">Kommentér</h4>
+					
+					<?php 
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) : ?>
+						
+						<div class="no-comments-yet">
+							<p class="text-sub text-mute">Ingen kommentarer til dette indlæg endnu.<br>Men måske du vil være den første?</p>
+
+							<button class="btn-neutral">Skriv kommentar</button>
+						</div>
+
+						<?php //comments_template();
+					endif; ?>
 				</div>
 			</div>
-			
-			<?php 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) : ?>
-				
-				<div class="no-comments-yet">
-					<p class="text-sub text-mute">Ingen kommentarer til dette indlæg endnu.<br>Men måske du vil være den første?</p>
 
-					<button class="btn-neutral">Skriv kommentar</button>
+			<div class="entry-actions entry-related row">
+				<div class="col-sm-12">
+					<hr>
+					<h4 class="entry-actions-header">Læs relateret indlæg</h4>
+
+					<div class="row">
+						<div class="col-sm-6">Relateret indlæg</div>
+						<div class="col-sm-6">Relateret indlæg</div>
+						<div class="col-sm-6">Relateret indlæg</div>
+						<div class="col-sm-6">Relateret indlæg</div>
+					</div>			
 				</div>
+			</div>
 
-				<?php //comments_template();
-			endif; ?>
-
-			<hr>
-
-			<h4>Relaterede artikler</h4>
-
-			<div class="empty"></div>
 
 		<?php
 		endwhile; // End of the loop.
