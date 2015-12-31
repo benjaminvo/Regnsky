ca2c5eb0c37fc5153d83898e4464f14592aa03c5
Benjamin Ottensten <benjamin@ungtriumf.dk>
2015-12-31 01:38:30 +0100

    Began styling the latest comments in sidebar

--- sidebar.php
+++ sidebar.php
@@ -24,17 +24,19 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 
     <p class="sidebar-site-description">Regnsky er en lille, dansk musikblog, der har eksisteret siden 2008. Her skriver unge skribenter om nyt musik gennem nyheder, anmeldelser og livereportager. <a class="h4" href="<?php echo esc_url( home_url( '/om-regnsky/' ) ); ?>">Læs mere</a></p>
 
-    <div class="sidebar-fb">
-        <span class="fb-follow-text text-sub">Følg Regnsky på Facebook:</span>
-        <div class="fb-like" data-href="https://www.facebook.com/regnsky.dk/" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
-    </div>
-
     <div class="social-buttons">
         <a href="#" onclick="return false;"><img class="social-btn social-btn-rss" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icon-rss_black.png" alt="RSS Feed"></a>
         <a href="#" onclick="return false;"><img class="social-btn social-btn-fb" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icon-fb_black.png" alt="Regnsky on Facebook"></a>
         <a href="#" onclick="return false;"><img class="social-btn social-btn-twitter" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icon-twitter_black.png" alt="Regnsky on twitter"></a>
         <a href="#" onclick="return false;"><img class="social-btn social-btn-hypem" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icon-hypem_black.png" alt="Regnsky on Hype Machine"></a>
     </div>
+
+    <div class="sidebar-fb">
+        <span class="fb-follow-text text-sub">Følg Regnsky på Facebook:</span>
+        <div class="fb-like" data-href="https://www.facebook.com/regnsky.dk/" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+    </div>
+
+
 
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
