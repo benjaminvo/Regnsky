db920759f814ceefaf0a6b58c6e34e8de38e8079
Benjamin Ottensten <benjamin@ungtriumf.dk>
2016-01-02 23:22:40 +0100

    PNG borders instead of SVG

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
     </div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<img class="bumpy-top_on-top" src="<?php echo get_stylesheet_directory_uri(); ?>/img/kant-footer.svg" alt="Footer kant">
+		<img class="bumpy-top_on-top" src="<?php echo get_stylesheet_directory_uri(); ?>/img/kant-footer.png" alt="Footer kant">
 
         <div class="site-info container">
 
