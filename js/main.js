2275392b0b7b22b9171a73354afd69b8c87426c9
Benjamin Ottensten <benjamin@ungtriumf.dk>
2016-04-03 22:45:09 +0200

    Main.js is now in use

--- js/main.js
+++ js/main.js
@@ -3,17 +3,22 @@
  *
  */
 
-( function() {
-    
+$(document).ready(function() {
+
+    // Navigation - toggle body class when menu is active
+    // $('.menu-toggle').on("click", function() {
+    //     $('body').toggleClass("nav-toggled");
+    // });
+   
     // Hide and display comments
     var btn_comment, no_comments, comments;
     btn_comment = document.getElementById( 'btn-comment' );
     no_comments = document.getElementById( 'no-comments' );
-    comments = document.getElementById( 'comments' );
+    comments = document.getElementById( 'comments-area_no-comments' );
 
     btn_comment.onclick = function() {
         no_comments.style.display = "none";
         comments.style.display = "block";
     };
 
-} )();
+});
