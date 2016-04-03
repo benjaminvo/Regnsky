8040e43dfe65181e1bd361e86b26c13e39bdb074
Benjamin Ottensten <benjamin@ungtriumf.dk>
2016-04-03 22:26:48 +0200

    Load main.js

--- functions.php
+++ functions.php
@@ -129,11 +129,11 @@ function regnsky_scripts() {
 	$jquery_cdn = '//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js';
 	wp_enqueue_script( 'jquery', $jquery_cdn, array(), '20130115', true );
 
+	wp_enqueue_script( 'regnsky-script', get_template_directory_uri() . '/js/main.js', array(), '20160401', true );
+
 	wp_enqueue_style( 'regnsky-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'regnsky-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'regnsky-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	
+	wp_enqueue_script( 'regnsky-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', false );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
