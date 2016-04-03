bc1df3fbd444ceb44d3fe94c4054330dec0d9514
Benjamin Ottensten <benjamin@ungtriumf.dk>
2016-04-03 22:27:34 +0200

    Scripts for sticky header

--- footer.php
+++ footer.php
@@ -17,15 +17,7 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<img class="bumpy-top_on-top" src="<?php echo get_stylesheet_directory_uri(); ?>/img/kant-footer.png" alt="Footer kant">
 
-        <div class="site-info container">
-
-            <nav class="footer-navigation" role="navigation">
-                <?php 
-                    wp_nav_menu( array( 'theme_location' => 'menu-pages', 'menu_id' => 'menu-pages' ) ); 
-                    wp_nav_menu( array( 'theme_location' => 'menu-categories', 'menu_id' => 'menu-categories' ) ); 
-                ?>
-            </nav>
-    
+        <div class="site-info container">    
             <p class="text-sub text-mute">
                 Copyright © Regnsky-skribenterne 2016<br>
                 Hjemmeside af <a href="ungtriumf.dk">Benjamin Vedel Ottensten</a><br>
@@ -39,35 +31,36 @@
 
 <script>
     
-    var headerPosition = $('.site-header').offset();
+    var windowWidth = $(window).width();
+    var header = $('.site-header');
+    var headerPos = header.offset();
 
-    $(window).scroll(function(){
-        if($(window).scrollTop() >= headerPosition.top){
-            // $('.site-header').css({'position':'fixed','top':'0'});
-            $('.site-header').addClass("stick");
-            $('#content').css({'margin-top':'165px'});
+    // Recalculate window width when window is resized
+    $(window).on('resize', function() {
+        windowWidth = $(window).width();
+    });
+
+    // Stick header
+    $(window).scroll(function() {
+        var windowPos = $(window).scrollTop();
+
+        if (windowPos >= 86 && windowWidth < 750) {             //  86 = banner height ((196 / 2) - header offset (-12)
+            header.addClass("stick");
+            $('#content').css({'margin-top':'166px'});          // 166 = header (80) + banner height (196/2) - header offset (-12)
+        } else if (windowPos >= 172 && windowWidth >= 750) {    // 172 = banner (196) - header offset (-12)
+            header.addClass("stick");
+            $('#content').css({'margin-top':'292px'});          // 292 = header (120) + banner height (196) - header offset (-24)
         } else {
-            // $('.site-header').css({'position':'static'});
-            $('.site-header').removeClass("stick");
+            header.removeClass("stick");
             $('#content').css({'margin-top':'0px'});
         }
     });
 
-    // var headerPos = header.offset();
-            
-    // $(window).scroll(function() {
-    //     var windowpos = $(window).scrollTop();
-        
-    //     if (windowpos >= header.pos) {
-    //         header.addClass("stick");
-    //     } else {
-    //         header.removeClass("stick"); 
-    //     }
-    // });
-
     // Navigation - toggle body class when menu is active
     $('.menu-toggle').on("click", function() {
-        $('body').toggleClass("menu-toggled");
+        $('body').toggleClass("nav-toggled");
+
+        // $('.main-navigation li').fadeIn();
     });
 
 </script>
