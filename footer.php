265a3f034e0e6abe534f776e5cdfef791383c461
Benjamin Ottensten <benjamin@ungtriumf.dk>
2016-04-07 11:04:53 +0200

    Only toggle menu on screen sizes below $md

--- footer.php
+++ footer.php
@@ -33,10 +33,12 @@
     
     var $window = $(window);
     var windowWidth = $window.width();
+    var bp_md = 750; // Breakpoint: md
     var $header = $('.site-header');
     var headerPos = $header.offset();
-    var $content = $('#content');
     var $menuItems = $('.main-navigation li');
+    var $content = $('#content');
+
 
     // Recalculate window width when window is resized
     $window.on('resize', function() {
@@ -47,10 +49,10 @@
     $window.on('scroll', function() {
         var windowPos = $window.scrollTop();
 
-        if (windowPos >= 86 && windowWidth < 750) {             //  86 = banner height ((196 / 2) - header offset (-12)
+        if (windowPos >= 86 && windowWidth < bp_md) {             //  86 = banner height ((196 / 2) - header offset (-12)
             $header.addClass("stick");
             $content.css({'margin-top':'166px'});          // 166 = header (80) + banner height (196/2) - header offset (-12)
-        } else if (windowPos >= 172 && windowWidth >= 750) {    // 172 = banner (196) - header offset (-12)
+        } else if (windowPos >= 172 && windowWidth >= bp_md) {    // 172 = banner (196) - header offset (-12)
             $header.addClass("stick");
             $content.css({'margin-top':'292px'});          // 292 = header (120) + banner height (196) - header offset (-24)
         } else {
@@ -70,29 +72,32 @@
         }
     });
 
-    // Navigation - toggle body class when menu is active
-    $('.menu-toggle').on("click", function() {
-        $('body').toggleClass("nav-toggled");
+    // Navigation: On small screen sizes, toggle body class when menu is active
+    if (windowWidth < bp_md) {
 
-        // Show/hide list elements
-        if (!$('body').hasClass('nav-toggled')) {
-            $menuItems.addClass('animated fadeOutUp');
-            $menuItems.removeClass('animated fadeOutUp');
-            // $(el).animateCss('fadeOut');
-        } else {
-            $.each($menuItems, function(i, el) { // Link: cl.ly/fajZ
+        $('.menu-toggle').on("click", function() {
+            $('body').toggleClass("nav-toggled");
 
-                // Add animate class one element at a time
-                setTimeout(function(){
-                   $(el).addClass('animated fadeInDown');
-                }, 60 + ( i * 30 ));
+            // Show/hide list elements
+            if (!$('body').hasClass('nav-toggled')) {
+                $menuItems.addClass('animated fadeOutUp');
+                $menuItems.removeClass('animated fadeOutUp');
+                // $(el).animateCss('fadeOut');
+            } else {
+                $.each($menuItems, function(i, el) { // Link: cl.ly/fajZ
 
-                // Remove animate class again
-                $(el).removeClass('animated fadeInDown')
+                    // Add animate class one element at a time
+                    setTimeout(function(){
+                       $(el).addClass('animated fadeInDown');
+                    }, 60 + ( i * 30 ));
 
-            });
-        }
-    });
+                    // Remove animate class again
+                    $(el).removeClass('animated fadeInDown')
+
+                });
+            }
+        });
+    }
 
 </script>
 
