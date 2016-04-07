de789949b7c0d1c80737c9c8a019e6a7d0edc688
Benjamin Ottensten <benjamin@ungtriumf.dk>
2016-04-08 00:54:45 +0200

    The whole entry-footer now works!

--- template-parts/entry-footer.php
+++ template-parts/entry-footer.php
@@ -1,14 +1,16 @@
 <!-- Bottom of entry - Share, comment and read more -->
-<div class="entry-actions row">
+<div class="entry-actions flex">
     
     <!-- Share -->
-    <div class="entry-share col-sm-3">
+    <!-- <div class="entry-share col col-xs-12 col-sm-2 offset-sm-1 col-md-2 offset-md-0"> -->
+    <div class="entry-share col col-xs-12 col-sm-10 offset-sm-1 col-md-2 offset-md-0 col-lg-2 col-xl-3">
+
         <!-- Display first header on extra small screens, second header on larger -->
-        <h4 class="entry-actions-header visible-xs">Del og kommentér</h4>
-        <h4 class="entry-actions-header hidden-xs">Del</h4>
+        <h4 class="entry-actions-header hidden-md-up">Del og kommentér</h4>
+        <h4 class="entry-actions-header hidden-sm-down">Del</h4>
 
         <div class="share-buttons">
-            <div class="fb">
+            <div class="share-button fb">
                 
                 <div class="fb-share-button" 
                     data-href="<?php echo the_permalink(); ?>" 
@@ -20,7 +22,7 @@
                 </a> -->
             </div>
             
-            <div class="twitter">
+            <div class="share-button twitter">
                 
                 <a href="https://twitter.com/share" class="twitter-share-button"{count}>Tweet</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
                 <!-- <a href="#" onclick="return false;">
@@ -31,16 +33,20 @@
     </div>
 
     <!-- Comment -->
-    <div id="comment-section" class="entry-comment col-sm-9">
+    <div id="comment-section" class="entry-comment col col-xs-12 col-sm-10 offset-sm-1 col-md-8 offset-md-0 col-lg-6 col-xl-6">
         <!-- Do not display this header on extra small screens -->
-        <h4 class="entry-actions-header hidden-xs">Kommentér</h4>
+        <h4 class="entry-actions-header hidden-sm-down">Kommentér</h4>
         
         <?php 
         // If comments are open or we have at least one comment, load up the comment template.
         if ( comments_open() && get_comments_number()==0 ) : ?>
             
             <div id="no-comments" class="no-comments-yet">
-                <p class="text-sub text-mute">Ingen kommentarer endnu. Vil du være den første?</p>
+                
+                <!-- Små skærmstørrelsen: To linjer -->
+                <h5 class="no-comments-cta hidden-md-up">Ingen kommentarer endnu.<br>Vil du være den første?</h5>
+                <!-- Store skærmstørrelsen: Én linje -->
+                <h5 class="no-comments-cta hidden-sm-down">Ingen kommentarer endnu. Vil du være den første?</h5>
 
                 <button id="btn-comment" class="btn-neutral">Skriv kommentar</button>
             </div>
