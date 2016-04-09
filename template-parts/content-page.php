ba5b627bdc16e4c311a45023086aa8f8d5d88b8a
Benjamin Ottensten <benjamin@ungtriumf.dk>
2016-04-09 23:22:01 +0200

    Grid

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,32 +10,46 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php
-			the_content();
+	<div class="entry-wrapper">
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'regnsky' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+		<div class="container">
+	
+			<div class="flex">
 
-	<footer class="entry-footer">
-		<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Name of current post */
-					esc_html__( 'Edit %s', 'regnsky' ),
-					the_title( '<span class="screen-reader-text">"', '"</span>', false )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-		?>
-	</footer><!-- .entry-footer -->
+				<header class="entry-header col col-xs-12 col-sm-10 offset-sm-1">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				</header><!-- .entry-header -->
+
+				<div class="entry-content col col-xs-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-xl-6 offset-xl-3">
+					<?php
+						the_content();
+
+						wp_link_pages( array(
+							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'regnsky' ),
+							'after'  => '</div>',
+						) );
+					?>
+
+					
+					<?php
+						// Edit link
+						edit_post_link(
+							sprintf(
+								/* translators: %s: Name of current post */
+								esc_html__( 'Edit %s', 'regnsky' ),
+								the_title( '<span class="screen-reader-text">"', '"</span>', false )
+							),
+							'<span class="edit-link">',
+							'</span>'
+						);
+					?>
+				</div><!-- .entry-content -->
+
+			</div>
+
+		</div> <!-- /.container -->
+
+	</div>
+
 </article><!-- #post-## -->
