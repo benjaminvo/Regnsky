550c605d5717cec29e26c4c6b53fe5edce301726
Benjamin Ottensten <benjamin@ungtriumf.dk>
2016-04-15 19:34:58 +0200

    Disable Wordpress embed plugin

--- functions.php
+++ functions.php
@@ -318,3 +318,25 @@ function get_excerpt($count) {
     $excerpt = $excerpt.'<span class="text-mute">&#8230; </span><a class="excerpt-read-more" href="'.$permalink.'">Læs indlæg</a>';
     return $excerpt;
 }
+
+/**
+ * Disable Wordpress Embed plugin
+ * https://wordpress.org/support/topic/how-to-disable-auto-wordpress-emmed-script
+ */
+function disable_embeds_init() {
+
+    // Remove the REST API endpoint.
+    remove_action('rest_api_init', 'wp_oembed_register_route');
+
+    // Turn off oEmbed auto discovery.
+    // Don't filter oEmbed results.
+    remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
+
+    // Remove oEmbed discovery links.
+    remove_action('wp_head', 'wp_oembed_add_discovery_links');
+
+    // Remove oEmbed-specific JavaScript from the front-end and back-end.
+    remove_action('wp_head', 'wp_oembed_add_host_js');
+}
+
+add_action('init', 'disable_embeds_init', 9999);
