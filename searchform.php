dbdba3d8a42650ee56df9fcd00c6fa683c978c1a
Benjamin Ottensten <benjamin@ungtriumf.dk>
2016-04-17 17:48:38 +0200

    Make search function work (added value and name properties)

--- searchform.php
+++ searchform.php
@@ -1,8 +1,10 @@
-<form role="search" method="get" class="search-form col col-xs-12 col-sm-10 offset-sm-1 col-md-6 offset-md-3 col-lg-4 offset-lg-4" action="<?php echo home_url( '/' ); ?>">
+<form role="search" method="get" class="search-form col col-xs-12 col-sm-10 offset-sm-1 col-md-6 offset-md-3 col-lg-4 offset-lg-4" action="<?php echo home_url( '/?s=' ); ?>">
     <label>
         <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
         <input type="search" class="search-field"
             placeholder="<?php echo esc_attr_x( 'Søg...', 'placeholder' ) ?>"
+            value="<?php echo get_search_query(); ?>"
+            name="s" id="s"
             title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
     </label>
     <span class="search-submit-wrap">
