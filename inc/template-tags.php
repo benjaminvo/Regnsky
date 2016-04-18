b24906080f0ed659fec9ec53f685e6a20fc54918
Benjamin Ottensten <benjamin@ungtriumf.dk>
2016-04-18 18:14:46 +0200

    Outcommented unused stuff

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -13,9 +13,9 @@ if ( ! function_exists( 'regnsky_posted_on' ) ) :
  */
 function regnsky_posted_on() {
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-	}
+	// if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+	// 	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+	// }
 
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
@@ -83,40 +83,40 @@ endif;
  *
  * @return bool
  */
-function regnsky_categorized_blog() {
-	if ( false === ( $all_the_cool_cats = get_transient( 'regnsky_categories' ) ) ) {
-		// Create an array of all the categories that are attached to posts.
-		$all_the_cool_cats = get_categories( array(
-			'fields'     => 'ids',
-			'hide_empty' => 1,
-			// We only need to know if there is more than one category.
-			'number'     => 2,
-		) );
+// function regnsky_categorized_blog() {
+// 	if ( false === ( $all_the_cool_cats = get_transient( 'regnsky_categories' ) ) ) {
+// 		// Create an array of all the categories that are attached to posts.
+// 		$all_the_cool_cats = get_categories( array(
+// 			'fields'     => 'ids',
+// 			'hide_empty' => 1,
+// 			// We only need to know if there is more than one category.
+// 			'number'     => 2,
+// 		) );
 
-		// Count the number of categories that are attached to the posts.
-		$all_the_cool_cats = count( $all_the_cool_cats );
+// 		// Count the number of categories that are attached to the posts.
+// 		$all_the_cool_cats = count( $all_the_cool_cats );
 
-		set_transient( 'regnsky_categories', $all_the_cool_cats );
-	}
+// 		set_transient( 'regnsky_categories', $all_the_cool_cats );
+// 	}
 
-	if ( $all_the_cool_cats > 1 ) {
-		// This blog has more than 1 category so regnsky_categorized_blog should return true.
-		return true;
-	} else {
-		// This blog has only 1 category so regnsky_categorized_blog should return false.
-		return false;
-	}
-}
+// 	if ( $all_the_cool_cats > 1 ) {
+// 		// This blog has more than 1 category so regnsky_categorized_blog should return true.
+// 		return true;
+// 	} else {
+// 		// This blog has only 1 category so regnsky_categorized_blog should return false.
+// 		return false;
+// 	}
+// }
 
 /**
  * Flush out the transients used in regnsky_categorized_blog.
  */
-function regnsky_category_transient_flusher() {
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-		return;
-	}
-	// Like, beat it. Dig?
-	delete_transient( 'regnsky_categories' );
-}
-add_action( 'edit_category', 'regnsky_category_transient_flusher' );
-add_action( 'save_post',     'regnsky_category_transient_flusher' );
+// function regnsky_category_transient_flusher() {
+// 	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+// 		return;
+// 	}
+// 	// Like, beat it. Dig?
+// 	delete_transient( 'regnsky_categories' );
+// }
+// add_action( 'edit_category', 'regnsky_category_transient_flusher' );
+// add_action( 'save_post',     'regnsky_category_transient_flusher' );
