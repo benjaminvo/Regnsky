6b178dfa23a4dce5afb44ebc22b9656a00aa00b8
Benjamin Ottensten <benjamin@ungtriumf.dk>
2016-07-07 09:27:46 +0200

    New code for scroll detection

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@
 
     var $window = $(window);
     var windowWidth = $window.width();
-    var bp_md = 880; // Breakpoint: md
+    var bp_md   = 750; // Breakpoint: md - her bliver headeren højere
+    var bp_nav  = 880;  // Breakpoint: burger menu --> navigation for, hvornår menuen bliver til en navigation
     var $header = $('.site-header');
     var headerPos = $header.offset();
     var $menuToggle = $('.menu-toggle');
@@ -21,36 +22,75 @@
     });
 
     // Stick header
-    $window.on('load scroll', function() {
-        var windowPos = $window.scrollTop();
+    // $window.on('load scroll', function() {
+    //     var windowPos = $window.scrollTop();
 
-        if (windowPos >= 86 && windowWidth < bp_md) {             //  86 = banner height ((196 / 2) - header offset (-12)
+    //     if (windowPos >= 86 && windowWidth < bp_md) {             //  86 = banner height ((196 / 2) - header offset (-12)
+    //         $header.addClass("stick");
+    //         $content.css({'margin-top':'166px'});          // 166 = header (80) + banner height (196/2) - header offset (-12)
+    //     } else if (windowPos >= 172 && windowWidth >= bp_md) {    // 172 = banner (196) - header offset (-12)
+    //         $header.addClass("stick");
+    //         $content.css({'margin-top':'272px'});          // 272 = header (120) + banner height (196) - header offset (-24)
+    //     } else {
+    //         $header.removeClass("stick");
+    //         $content.css({'margin-top':'0px'});
+    //     }
+    // });
+
+    // $(document).on('scroll', _.throttle(function(){
+    //     var windowPos = $window.scrollTop();
+
+    //     if (windowPos >= 86 && windowWidth < bp_md) {             //  86 = banner height ((196 / 2) - header offset (-12)
+    //         $header.addClass("stick");
+    //         $content.css({'margin-top':'166px'});          // 166 = header (80) + banner height (196/2) - header offset (-12)
+    //     } else if (windowPos >= 172 && windowWidth >= bp_md) {    // 172 = banner (196) - header offset (-12)
+    //         $header.addClass("stick");
+    //         $content.css({'margin-top':'272px'});          // 272 = header (120) + banner height (196) - header offset (-24)
+    //     } else {
+    //         $header.removeClass("stick");
+    //         $content.css({'margin-top':'0px'});
+    //     }
+    // }, 16));
+
+
+    var latestKnownScrollY = 0,
+        ticking = false;
+
+    function update() {
+        // reset the tick so we can
+        // capture the next onScroll
+        ticking = false;
+
+        if (latestKnownScrollY >= 86 && windowWidth < bp_md) {             //  86 = banner height ((196 / 2) - header offset (-12)
             $header.addClass("stick");
-            $content.css({'margin-top':'166px'});          // 166 = header (80) + banner height (196/2) - header offset (-12)
-        } else if (windowPos >= 172 && windowWidth >= bp_md) {    // 172 = banner (196) - header offset (-12)
+            $content.css({'margin-top':'166px'});                          // 166 = header (80) + banner height (196/2) - header offset (-12)
+        } else if (latestKnownScrollY >= 172 && windowWidth >= bp_md) {    // 172 = banner (196) - header offset (-12)
             $header.addClass("stick");
-            $content.css({'margin-top':'272px'});          // 272 = header (120) + banner height (196) - header offset (-24)
+            $content.css({'margin-top':'272px'});                          // 272 = header (120) + banner height (196) - header offset (-24)
         } else {
             $header.removeClass("stick");
             $content.css({'margin-top':'0px'});
         }
-    });
+    }
 
-    // Extend jQuery with animation function
-    // NOT IN USE
-    // $.fn.extend({
-    //     animateCss: function (animationName) {
-    //         var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-    //         $(this).addClass('animated ' + animationName).one(animationEnd, function() {
-    //             $(this).removeClass('animated ' + animationName);
-    //         });
-    //     }
-    // });
+    function onScroll() {
+        latestKnownScrollY = window.scrollY;
+        requestTick();
+    }
+
+    function requestTick() {
+        if(!ticking) {
+            requestAnimationFrame(update);
+        }
+        ticking = true;
+    }
+
+    window.addEventListener('scroll', onScroll, false);
 
     // Navigation
     $menuToggle.on("click", function(e) {
-        
-        if (windowWidth < bp_md) {
+
+        if (windowWidth < bp_nav) {
             toggleMenu();
         }
         // e.preventDefault();
@@ -62,14 +102,14 @@
         if ($header.hasClass('active')) {
             $header.removeClass("active");
             $menuToggle.removeClass("active");
-            
+
             $menu.hide();
             $menuItems.addClass('animated fadeOutUp');
             $menuItems.removeClass('animated fadeOutUp');
         } else {
             $header.addClass("active");
             $menuToggle.addClass("active");
-            
+
             $menu.show();
 
             // Add fade in classes one by one
@@ -96,7 +136,7 @@
     var postId, $comments_cta, $comments_form;
 
     function displayComments(postId) {
-        
+
         // Get post ID, use it to only target comment content relevant to that specific post
         $comments_cta  = $(postId + ' .comments-cta');
         $comments_form = $(postId + ' .comment-respond');
